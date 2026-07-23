48a75597b63c48341fad6dc155750583183ca986
wdpspace <wdp.space@outlook.com>
2026-07-23 22:39:12 +0400

    db connect

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php
-echo "The connection from my GitHub Repo works!";
-echo " <br>";
-echo "This is through VSCode.dev";
-echo "<br>";
-echo "Another test";
-echo "<br>";
-echo "Testing the webhook";
-echo "<br>";
-echo "another webhook test just to be safe";
+
