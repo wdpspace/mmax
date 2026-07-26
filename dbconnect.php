3fb7b7e758f60ffb3eb69f8ed9b3b00fb8585b3a
wdpspace <wdp.space@outlook.com>
2026-07-26 21:58:38 +0400

    q

--- dbconnect.php
+++ dbconnect.php
@@ -4,15 +4,16 @@
     $db_user = "wdpspace_me1";
     $db_password = "Hobbit$$7";
     $db_name = "wdpspace_mmax";
+    $conn = "";
     
     try {
     $conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
     }
     catch (mysqli_sql_exception) {
-        echo "Could not connect to database";
+        echo "Could not connect to database.";
     }
 
     if($conn){
-        echo "Connection succesful";
+        echo "Connection succesful!";
     }
    
