9e845615026187fc5326af199022b57cef62703d
wdpspace <wdp.space@outlook.com>
2026-07-26 22:00:50 +0400

    q

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
+    include ("dbconnect.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello <br>
+</body>
+</html>
 
