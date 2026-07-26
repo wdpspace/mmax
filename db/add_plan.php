37ce07d898e98d2ea433fe0ef16bb40fff162912
wdpspace <wdp.space@outlook.com>
2026-07-26 22:48:32 +0400

    q

--- db/add_plan.php
+++ db/add_plan.php
@@ -8,19 +8,18 @@
 </head>
 <body>
 <div class="container">
-<form>
+<form method="post">
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <label>Country</label>
+    <input type="text" class="form-control">
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label>Plan Name</label>
+    <input type="text" class="form-control">
   </div>
   <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <input type="number" class="form-control">
+    <label>Fee</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
