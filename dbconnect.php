d00376b09a34a8cdc2e6609e7288b48627e1fb61
wdpspace <wdp.space@outlook.com>
2026-07-26 21:52:04 +0400

    update

---
+++ dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+
+    $db_server = "localhost:3306";
+    $db_user = "wdpspace_me";
+    $db_password = "Hobbit$$7";
+    $db_name = "wdpspace_mmax";
+    
+    $conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+    if($conn){
+        echo "Connection succesful";
+    }
+    else{
+        echo "Could not connect to database";
+    }
--- index.php
+++ index.php
@@ -1,18 +1,2 @@
 <?php
 
-require "config.php";
-
-$sql = "SELECT * FROM plans";
-$result = $conn->query($sql);
-
-// Print all rows
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "Carrier: " . $row["gen_carrier"] . "<br>";
-        echo "Category: " . $row["gen_category"] . "<br>";
-        echo "Contract Term: " . $row["gen_contract_term"] . "<br>";
-        echo "<hr>";
-    }
-} else {
-    echo "No records found.";
-}
