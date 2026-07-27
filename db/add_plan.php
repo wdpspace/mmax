ecd46f688ca6149dc0f36be7354e8c4f9a374be9
wdpspace <wdp.space@outlook.com>
2026-07-27 21:36:48 +0400

    q

--- db/add_plan.php
+++ db/add_plan.php
@@ -13,13 +13,13 @@
 </head>
 <body>
 
-<form method="post" action="">
+<form method="post" action="add_plan.php">
 
 <label for="country">Country:</label>
 <input type="text" name="country" id="country">
 <br><br>
 
-<label for="">Plan name:</label>
+<label for="plan_name">Plan name:</label>
 <select name="plan_name" id="plan_name">
   <option value="postpaid">Postpaid</option>
   <option value="prepaid">Prepaid</option>
