7d3b5f32f655e3be37030c182ee759e5b3f0cafe
wdpspace <wdp.space@outlook.com>
2026-07-29 12:33:29 +0400

    q

--- db/plan_read.php
+++ db/plan_read.php
@@ -42,7 +42,7 @@
                             <td>{$country}</td>
                             <td>{$name}</td>
                             <td>{$fee}</td>
-                            <td> <a href='plan_update.php?id={$id}'>Delete</a>&nbsp;&nbsp;<a href='plan_delete.php?id={$id}'>Delete</a></td>                 
+                            <td> <a href='plan_update.php?id={$id}'>Update</a>&nbsp;&nbsp;<a href='plan_delete.php?id={$id}'>Delete</a></td>                 
                         </tr>";
                 }
             }
