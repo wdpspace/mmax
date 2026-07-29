3c6a4c90703c98579a99eb44de8477a5e1caf881
wdpspace <wdp.space@outlook.com>
2026-07-29 22:03:08 +0400

    q

---
+++ db/plan_delete.php
@@ -0,0 +1,18 @@
+<?php
+
+include ("dbconnect.php");
+
+if(isset($_GET("id"))){
+   $id = $_GET["id"];
+   
+   $sql = "DELETE FROM test WHERE id = $id";
+
+   $result = mysqli_query($conn, $sql);
+
+   if($result){
+    echo "Delete successful";
+   }
+   else{
+    exit("Could not delete. <br>");
+   }
+}
