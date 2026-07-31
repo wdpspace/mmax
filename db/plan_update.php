0d08ac65296bec9e9ce7b59597b171deaf622fce
wdpspace <wdp.space@outlook.com>
2026-07-31 22:25:31 +0400

    q

--- db/plan_update.php
+++ db/plan_update.php
@@ -9,7 +9,7 @@
       $country = $row["country"];
       $plan_name = $row["plan_name"];
       $fee = $row["fee"];
-    
+    vardump($row);
 
     if(isset($_POST["submit"])){
       $id = $_GET["id"];
