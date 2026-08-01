ff3dabc2eeaaec3c13e3bc2ae1db112b8be2dcad
wdpspace <wdp.space@outlook.com>
2026-08-01 16:07:56 +0400

    q

--- index.php
+++ index.php
@@ -35,7 +35,10 @@
                     if($result){
                         while($row = mysqli_fetch_assoc($result))
                             {
-                                echo "there is a row";
+                                echo "<tr>";
+                                echo "<td> $row["id"] </td>"; 
+
+                                echo "</tr>"
                             };
                         //continue from here
                     }
