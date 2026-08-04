84580ccdcf4bb5e1960d983e05602ca44313a5a5
wdpspace <wdp.space@outlook.com>
2026-08-04 22:33:25 +0400

    q

---
+++ admin/create.php
@@ -0,0 +1,37 @@
+<?php
+
+require ("dbconnect.php");
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Plan</title>
+</head>
+<body>
+    <h1>Add New Plan</h1> 
+
+    <form action="">
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </form>
+
+
+
+</body>
+</html>
