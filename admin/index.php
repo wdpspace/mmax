43403b97a9c587729594ce3f4f94c6d0d7be4a4e
wdpspace <wdp.space@outlook.com>
2026-08-05 22:01:18 +0400

    q

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,15 @@
+<?php
+
+require ("dbconnect.php");
+
+$sql = "SELECT * FROM plans";
+$conn = dbconnect();
+$result = mysqli_query($conn, $sql);
+
+$fields = mysqli_fetch_fields($result);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +19,32 @@
 </head>
 <body>
     <h1>Plan Summary</h1><br><br>
-    <a href="create.php">Add new plan</a>
+    <a href="create.php">Add new plan</a><br><br>
+
+    <form action="">
+        <table>
+            <thead>
+                <tr>
+                    <?php foreach($fields as $field){
+                            echo "<th>" . $field->name . "</th>";
+                        }
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                    <?php
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr>";
+                            foreach ($row as $value){
+                                echo "<td>$value</td>";
+                            }
+                        echo "</tr>";
+                    }
+
+                    ?>
+            
+            </tbody>
+        </table>
+    </form>
 </body>
 </html>
