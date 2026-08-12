1393cd341d50834825668c876567e4dfd8a6fb65
wdpspace <wdp.space@outlook.com>
2026-08-12 21:33:25 +0400

    q

---
+++ admin/play.php
@@ -0,0 +1,12 @@
+<?php
+
+require ("dbconnect.php");
+
+$sql = "SELECT * FROM plans";
+$conn = dbconnect();
+
+$result = mysqli_query($conn, $sql)
+
+$row = mysqli_fetch_fields($result);
+
+echo "$row";
