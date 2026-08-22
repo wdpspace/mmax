e049d7949bc041cf40c10672f34560873bea6314
wdpspace <wdp.space@outlook.com>
2026-08-22 14:14:22 +0400

    q

--- admin/play.php
+++ admin/play.php
@@ -7,8 +7,9 @@ $conn = dbconnect();
 // Preload all dropdown data into a DROPDOWN array
 
 $queries = [
-    'carrier' => 'SELECT * FROM opt_carrier',
-    'country' => 'SELECT * FROM opt_country'
+    'gen_carrier' => 'SELECT carrier FROM opt_carrier',
+    'gen_country' => 'SELECT country FROM opt_country',
+    'gen_currency' => 'SELECT currency FROM opt_country',
     // Add addtional dropdown options here ...
 ];
 
@@ -17,7 +18,7 @@ $dropdown = [];
 foreach($queries as $key => $sql){
     $result = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_assoc($result)){
-        $dropdown[$key][] = $row;
+        $dropdown[$key] = $row;
     }
 }
 
