79dba153c412ebc40f25cbea1750e276d518dd89
wdpspace <wdp.space@outlook.com>
2026-08-23 20:38:18 +0400

    q

--- play/play.php
+++ play/play.php
@@ -32,7 +32,7 @@ foreach($queries as $key => $sql){
 }
 
 foreach($fields as $field) {
-    if(array_key_exists($field->name, $dropdown)){ // use isset instead?
+    if(isset($dropdown[$field->name])){ // use isset instead?
         echo $field->name. " is a dropdown. <br>";
     } else {
         echo $field->name. " is an input. <br>";
