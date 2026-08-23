79a33dc1cac97f7da21b6a1e7d5684581d9b45c4
wdpspace <wdp.space@outlook.com>
2026-08-23 22:48:30 +0400

    q

--- admin/mapping.php
+++ admin/mapping.php
@@ -42,5 +42,205 @@ $mapping_create = [
         'comment' => ''
     ]
     
+    'gen_plan_name' => [
+        'visible' => true,
+        'comment' => ''
+    ],
 
-];
+    'gen_contract_term' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+    'gen_partner_offer' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'gen_premium_number' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+    'gen_url' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'fee_currency' => [
+        'visible' => false,
+        'comment' => ''
+    ]
+    'fee_rate' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'fee_vat' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+---------------
+    'fee_period' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'fee_activation' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'fee_other' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'fee_promo_available' => [
+        'visible' => true,
+        'comment' => 'Optional'
+    ]
+
+    'fee_promo_rate' => [
+        'visible' => true,
+        'comment' => 'Optional'
+    ]
+
+    'fee_promo_period' => [
+        'visible' => true,
+        'comment' => 'Optional'
+    ],
+
+    'data_national' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'data_bonus' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'data_speed' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'data_rollover_available' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'data_rollover' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'data_uaewifi_available' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'data_uaewifi_uom' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'data_uaewifi_value' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'minutes_type' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'minutes_total' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'minutes_national' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'minutes_international' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'minutes_one_country' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'minutes_one_number' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'roam_available' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'roam_package_name' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'roam_minutes' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'roam_data' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'roam_gcc' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'ent_available' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'ent_eligible' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'ent_disney' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'ent_prime' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'ent_shahid' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'ent_osn' => [
+        'visible' => true,
+        'comment' => ''
+    ]
+
+    'ent_starzplay' => [
+        'visible' => true,
+        'comment' => ''
+    ],
+
+    'ent_comment' => [
+        'visible' => true,
+        'comment' => ''
+    ];
+
